41aff682a5748de2b296c733c6286e7e7247efcb
ceci-basti <cecilia@cryptus.app>
2026-03-01 02:23:28 -0300

    chore: cambios recientes en layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
-import SessionWrapper from "@/components/sessionWrapper";
+//import SessionWrapper from "@/components/sessionWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,11 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SessionWrapper>
         <Providers>
           {children}
         </Providers>
-        </SessionWrapper>
       </body>
     </html>
   );

d5066508d083bb29a9f05415409423e077f0a015
ceci-basti <cecilia@cryptus.app>
2026-03-01 00:58:48 -0300

    fix: clean dbconnect for vercel

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,58 +1,26 @@
 
 import mongoose from 'mongoose';
 
-// Extiende el tipo global para incluir mongoose
-declare global {
-  // eslint-disable-next-line no-var
-  var mongoose: {
-    conn: typeof mongoose | null;
-    promise: Promise<typeof mongoose> | null;
-  } | undefined;
-}
-
-
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
   throw new Error('Por favor, define la variable MONGODB_URI en Vercel');
 }
 
-/** * Global se usa aquí para mantener la conexión cacheada 
- * a través de las recargas en desarrollo y las funciones serverless de Vercel.
- */
+declare global {
+  // eslint-disable-next-line no-var
+  var mongooseConn: Promise<typeof mongoose> | undefined;
+}
 
-let cached = global.mongoose;
+let cached = global.mongooseConn;
 
-// Ensure 'cached' is defined before use
 if (!cached) {
-  cached = { conn: null, promise: null };
-  global.mongoose = cached;
+  cached = mongoose.connect(MONGODB_URI, { bufferCommands: false });
+  global.mongooseConn = cached;
 }
 
 async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  // 'cached' está garantizado como definido aquí
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  
-  try {
-    cached.conn = await cached.promise;
-  } catch (e) {
-    cached.promise = null;
-    throw e;
-  }
-
-  return cached.conn;
+  return cached;
 }
 
 export default dbConnect;
